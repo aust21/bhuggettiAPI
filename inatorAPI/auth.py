f00a2d18d9e16e1972e94ac56c2af6a21c48b36d
Austin <kngobeni223@gmail.com>
2024-11-03 01:02:00 +0200

    reset password

--- inatorAPI/auth.py
+++ inatorAPI/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash, current_app
-import sys, os, requests, json, random, sqlite3
+import sys, os, requests, json, random
 from .models import User
 from . import db, mail
 from .views import views
@@ -7,6 +7,7 @@ from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from oauthlib.oauth2 import WebApplicationClient
 from flask_mail import Message
+from itsdangerous import URLSafeTimedSerializer
 
 sys.path.append(os.getcwd())
 
@@ -18,6 +19,94 @@ GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configura
 
 
 client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+# --------------------------Reset password-------------------------- #
+
+@auth.route("/forgot-password")
+def forgot():
+    return render_template("forgot_password.html")
+
+# Initialize serializer with your app's secret key
+def get_reset_token_serializer():
+    return URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+
+# Route for requesting password reset
+@auth.route("/forgot_password", methods=['GET', 'POST'])
+def forgot_password():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            # Generate secure token
+            serializer = get_reset_token_serializer()
+            reset_token = serializer.dumps(user.email, salt='password-reset-salt')
+            
+            # Create reset link
+            reset_url = url_for(
+                'auth.reset_password',
+                token=reset_token,
+                _external=True
+            )
+            
+            # Send email
+            msg = Message('Password Reset Request',
+                        sender=current_app.config['MAIL_USERNAME'],
+                        recipients=[user.email])
+            msg.html = render_template('email/reset_email.html', reset_url=reset_url)
+            mail.send(msg)
+            
+        # Always show this message even if email doesn't exist (prevents email enumeration)
+        flash('If an account exists with that email, a password reset link has been sent.', 'info')
+        return redirect(url_for('auth.login'))
+        
+    return redirect(url_for('auth.login'))
+
+
+# Route for resetting password with token
+@auth.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_password(token):
+    try:
+        # Verify token (expires after 3600 seconds = 1 hour)
+        serializer = get_reset_token_serializer()
+        email = serializer.loads(token, salt='password-reset-salt', max_age=3600)
+        user = User.query.filter_by(email=email).first()
+        
+        if not user:
+            flash('Invalid or expired reset link', 'error')
+            return redirect(url_for('auth.login'))
+            
+        if request.method == 'POST':
+            password = request.form.get('password')
+            confirm_password = request.form.get('confirmPassword')
+            
+            if not password or not confirm_password:
+                flash('Please fill in all fields', 'error')
+                return render_template('reset_password.html')
+                
+            if password != confirm_password:
+                flash('Passwords do not match', 'error')
+                return render_template('reset_password.html')
+                
+            if len(password) < 8:
+                flash('Password must be at least 8 characters long', 'error')
+                return render_template('reset_password.html')
+                
+            # Update password
+            user.password = generate_password_hash(password)
+            db.session.commit()
+            
+            flash('Your password has been updated!', 'success')
+            return redirect(url_for('auth.login'))
+            
+        return render_template('reset_password.html')
+        
+    except:
+        flash('Invalid or expired reset link', 'error')
+        return redirect(url_for('auth.login'))
+
+
+# --------------------------login-------------------------- #
 
 @auth.route("/login/google")
 def login_with_google():
@@ -86,51 +175,6 @@ def google_callback():
     login_user(user)
     return redirect(url_for("views.home"))
 
-def generate_code():
-    code = set()
-
-    while len(code) < 7:
-        num = random.randint(0, 9)
-        code.add(str(num))
-    return "".join(code)
-
-# 
-
-@auth.route("/confirm-account", methods=["GET", "POST"])
-def update_account():
-    if request.method == "POST":
-        code = request.form.get("code")
-        if current_user.cormfirm_code == code:
-            current_user.account_confirmed = True
-            db.session.commit()
-            flash("Account verified")
-            return redirect(url_for('views.settings', id=current_user.id))
-        flash("Account could not be confirmed")
-        return redirect(url_for('views.settings', id=current_user.id))
-    flash("An error has occured, please try again")
-    return redirect(url_for('views.settings', id=current_user.id))
-
-
-@auth.route("/verify", methods=["GET", "POST"]) 
-def verify():
-    code = generate_code() 
-    current_user.cormfirm_code = code
-    db.session.add(current_user)
-    db.session.commit()
-    
-    msg = Message('bhuggettiAPI | Email Verification',
-                  sender=current_app.config['MAIL_USERNAME'],
-                  recipients=[current_user.email])
-    
-    msg.html = render_template('email/verification.html', 
-                             user=current_user, 
-                             code=code)
-    
-    mail.send(msg)
-    flash('Verification code has been sent to your email.', 'success')
-    return redirect(url_for('views.settings', id=current_user.id))
-
-
 @auth.route("/complete-profile", methods=["GET", "POST"])
 @login_required
 def complete_profile():
@@ -178,6 +222,54 @@ def login():
 @auth.route("/reset-password")
 def reset():
     return render_template("reset.html")
+
+# --------------------------verify account-------------------------- #
+
+def generate_code():
+    code = set()
+
+    while len(code) < 7:
+        num = random.randint(0, 9)
+        code.add(str(num))
+    return "".join(code)
+
+# 
+
+@auth.route("/confirm-account", methods=["GET", "POST"])
+def update_account():
+    if request.method == "POST":
+        code = request.form.get("code")
+        if current_user.cormfirm_code == code:
+            current_user.account_confirmed = True
+            db.session.commit()
+            flash("Account verified")
+            return redirect(url_for('views.settings', id=current_user.id))
+        flash("Account could not be confirmed")
+        return redirect(url_for('views.settings', id=current_user.id))
+    flash("An error has occured, please try again")
+    return redirect(url_for('views.settings', id=current_user.id))
+
+
+@auth.route("/verify", methods=["GET", "POST"]) 
+def verify():
+    code = generate_code() 
+    current_user.cormfirm_code = code
+    db.session.add(current_user)
+    db.session.commit()
+    
+    msg = Message('bhuggettiAPI | Email Verification',
+                  sender=current_app.config['MAIL_USERNAME'],
+                  recipients=[current_user.email])
+    
+    msg.html = render_template('email/verification.html', 
+                             user=current_user, 
+                             code=code)
+    
+    mail.send(msg)
+    flash('Verification code has been sent to your email.', 'success')
+    return redirect(url_for('views.settings', id=current_user.id))
+
+# --------------------------sign up-------------------------- #
 
 
 @auth.route("/sign-up", methods=["GET", "POST"])
